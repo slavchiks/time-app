e5542195a67df86f1e520602029d1e9c56e707ba
slavchiks <sam.stanulis@yandex.ru>
2024-03-05 19:48:06 +0300

    Написал html страницу. Теперь можно выставлять определенную дату

--- internal/app/endpoint/endpoint.go
+++ internal/app/endpoint/endpoint.go
@@ -3,26 +3,40 @@ package endpoint
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
-type Service interface{
-	DaysLeft() int 
+type Service interface {
+	DaysLeft() int
+	DiffDays(time.Time) int
+	ParsDate(*http.Request) (time.Time, error)
 }
 
 type Endpoint struct {
 	s Service
 }
 
-func New(s Service) *Endpoint{
+func New(s Service) *Endpoint {
 	return &Endpoint{
-		s:s,
-	}//
+		s: s,
+	}
 }
 
+func (e *Endpoint) Status(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		http.ServeFile(w, r, "cmd\\time-task\\form.html")
+		d := e.s.DaysLeft()
+		fmt.Fprintf(w, "Days left : %d", d+1)
 
-func (e *Endpoint) Status(w http.ResponseWriter, r *http.Request){
-	if r.Method == http.MethodGet{
-		d:= e.s.DaysLeft()
-		fmt.Fprintf(w,"Days left : %d", d)
+	} else if r.Method == http.MethodPost {
+		date, err := e.s.ParsDate(r)
+
+		if err != nil {
+			http.Error(w, "Date parsing error!", http.StatusBadRequest)
+			return
+		}
+
+		res := e.s.DiffDays(date)
+		fmt.Fprintf(w, "Days left : %d", res+1)
 	}
 }
--- internal/app/service/service.go
+++ internal/app/service/service.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"net/http"
 	"time"
 )
 
@@ -16,4 +17,14 @@ func (s *Service) DaysLeft() int {
 	data := time.Date(2025, time.January, 1, 0, 0, 0, 0, time.UTC)
 	dur := time.Until(data)
 	return int(dur.Hours())/24
+}
+func (s *Service) DiffDays(data time.Time) int {
+	dur := time.Until(data)
+	return int(dur.Hours())/24
+}
+func (s *Service)ParsDate(r *http.Request)(time.Time, error){
+	dateStr := r.FormValue("date")
+	layout := "2006-01-02"
+	date, err := time.Parse(layout, dateStr)
+	return date, err
 }
