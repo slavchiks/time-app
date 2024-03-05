6c17cd3469e05a14987b7dacf8d489588a2b1695
slavchiks <sam.stanulis@yandex.ru>
2024-03-05 16:38:26 +0300

    Первоначальное состояние, когда все работает

--- internal/app/endpoint/endpoint.go
+++ internal/app/endpoint/endpoint.go
@@ -3,7 +3,6 @@ package endpoint
 import (
 	"fmt"
 	"net/http"
-	"time"
 )
 
 type Service interface{
@@ -23,21 +22,6 @@ func New(s Service) *Endpoint{
 
 func (e *Endpoint) Status(w http.ResponseWriter, r *http.Request){
 	if r.Method == http.MethodGet{
-		/*http.ServeFile(w, r, "form.html")
-		if r.Method == "POST" {
-            dateStr := r.FormValue("date")
-            layout := "2006-01-02"
-			date, err := time.Parse(layout, dateStr)
-				if err != nil {
-					http.Error(w, "Ошибка при парсинге даты", http.StatusBadRequest)
-					return
-				}
-				if date.After(time.Now()) {
-					fmt.Fprintf(w, "Введенная дата %s находится в будущем", dateStr)
-				} else {
-					fmt.Fprintf(w, "Введенная дата %s находится в прошлом или сегодня", dateStr)
-				}
-		}*/
 		d:= e.s.DaysLeft()
 		fmt.Fprintf(w,"Days left : %d", d)
 	}
